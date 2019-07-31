6a676f699bfc1967f9f8d05fe7648e44f3601f37
Anthony Diké <anthonydike@nyu.edu>
2019-07-31 17:26:17 -0400

    Create base of Timer component

---
+++ src/components/Timer.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class Timer extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This Is The Timer</h1>
+      </div>
+    );
+  }
+}
+
+export default Timer;
