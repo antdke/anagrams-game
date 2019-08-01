ea388366f1e7d22407c2cdd5ae9dbe73aac1003a
Anthony Diké <anthonydike@nyu.edu>
2019-08-01 18:35:29 -0400

    Renamed Words.tsx to BottomRow.tsx and commented the purpose of the component

---
+++ src/components/BottomRow.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+/**
+ * This component will:
+ * 1. Store the possible words in an array
+ * 2. Randomly pick a word from the array
+ * 3. Scramble the selected word
+ * 4. Display the scrambled, selected word in the bottom row
+ */
+
+const BottomRow = () => {
+  const wordBank = ["ACTORS", "ASCENT", "RENTAL"];
+  return (
+    <div>
+      <h1>This is where we store the Words</h1>
+    </div>
+  );
+};
+
+export default BottomRow;
