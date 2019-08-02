56d784c8815a935ad3f419cb564c2cea81a93172
Anthony Diké <anthonydike@nyu.edu>
2019-08-02 11:12:46 -0400

    Got the letter buttons to pass their letter values as props to the TopRow component

--- src/components/BottomRow.tsx
+++ src/components/BottomRow.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import TopRow from "./TopRow";
 
 /**
  * This component will:
@@ -41,12 +42,57 @@ const BottomRow = () => {
   let oneScrambledWord = scrambleWord(oneWord);
   //split scrambled word into an array of letters
   let scrambledLetters = oneScrambledWord.split("");
+  // an array to pass to TopRow and function to edit the array
+  //var lettersPassed = ["A", "B"];
+
+  //add letter to the front of the array and return the array
+  function AddLetterToFront(letter: string, lettersPassed: Array<string>) {
+    lettersPassed.splice(0, 0, letter);
+    console.log(lettersPassed);
+    return [...lettersPassed];
+  }
+
+  //give each letter its own state
+  const [firstLetter, setFirstLetter] = React.useState(scrambledLetters[0]);
+  const [secondLetter, setSecondLetter] = React.useState(scrambledLetters[1]);
+  const [thirdLetter, setThirdLetter] = React.useState(scrambledLetters[2]);
+  const [fourthLetter, setFourthLetter] = React.useState(scrambledLetters[3]);
+  const [fifthLetter, setFifthLetter] = React.useState(scrambledLetters[4]);
+  const [sixthLetter, setSixthLetter] = React.useState(scrambledLetters[5]);
+
+  // function handleClick(letter: string) {
+  //   lettersPassed = [letter, ...lettersPassed];
+  // }
+
+  const [lettersPassed, setLettersPassed] = React.useState("");
 
   return (
     <div>
-      {scrambledLetters.map(letter => (
-        <button>{letter}</button>
-      ))}
+      {/* What if the problem is that the lettersPassed value in .map is out of reach to the TopRow component???? */}
+      <TopRow letters={lettersPassed} />
+
+      {/*Instead of mapping the buttons and trapping the key value inside the map,
+      I'll just manually list them out since the letters are only 6 */}
+      <button onClick={() => setLettersPassed(firstLetter)}>
+        {firstLetter}
+      </button>
+      <button onClick={() => setLettersPassed(secondLetter)}>
+        {secondLetter}
+      </button>
+      <button onClick={() => setLettersPassed(thirdLetter)}>
+        {thirdLetter}
+      </button>
+      <button onClick={() => setLettersPassed(fourthLetter)}>
+        {fourthLetter}
+      </button>
+      <button onClick={() => setLettersPassed(fifthLetter)}>
+        {fifthLetter}
+      </button>
+      <button onClick={() => setLettersPassed(sixthLetter)}>
+        {sixthLetter}
+      </button>
+      {/*Debug: Seeing if a changed value can be passed */}
+      <button onClick={() => setLettersPassed("A")}> Test </button>
     </div>
   );
 };
--- src/components/TopRow.tsx
+++ src/components/TopRow.tsx
@@ -2,14 +2,18 @@ import React from "react";
 
 /**
  * This compenent will:
- * 1. Take props (letters) from BottomRow
+ * 1. Take props (letters array) from BottomRow
  * 2. Display the letter in an input field
  */
 
-const TopRow = () => {
+type TopRowProps = {
+  letters: string;
+};
+
+const TopRow: React.FC<TopRowProps> = ({ letters }) => {
   return (
     <div>
-      <h1>"This is the Top Row"</h1>
+      <h1>{letters}</h1>
     </div>
   );
 };
