d27a90d8e6bca4f7fd1afffde32a2a2b2c44aae9
Anthony Diké <anthonydike@nyu.edu>
2019-08-03 21:36:30 -0400

    Changed TopRow comments

--- src/components/TopRow.tsx
+++ src/components/TopRow.tsx
@@ -3,7 +3,7 @@ import React from "react";
 /**
  * This compenent will:
  * 1. Take props (letters string) from BottomRow
- * 2. Display the letter in an input field
+ * 2. Display the letters as buttons that are deletable
  */
 
 type TopRowProps = {
