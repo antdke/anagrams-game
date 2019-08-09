17c7db2d1814bcdb79a0936b578f62a8fdb998bc
Anthony Diké <anthonydike@nyu.edu>
2019-08-08 23:55:28 -0400

    Removed eyesore 'App Goes Here'

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>"App Goes Here"</h1>
         <Timer />
         <BottomRow />
       </header>
